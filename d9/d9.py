4f603f9cd80b776637efe045169a37bdf7ec7beb
Björn Eriksson <martorn@gmail.com>
2023-12-09 07:01:12 +0100

    yo

--- d9/d9.py
+++ d9/d9.py
@@ -4,29 +4,31 @@ from collections import defaultdict
 
 lines = open(sys.argv[1], "r").read().split("\n")
 
-seqs = [l.split(" ") for l in lines][:-1]
+seqs = []
+for line in lines[:-1]:
+    seqs.append([int(l) for l in line.split()])
+
+print(seqs)
 
 sum = 0
+sum2 = 0 
 for s in seqs:
     step = 0
-    D = dict()
-    D[step]=s
+    D = []
+    D.append(s)
     d = s
     while not [0] * len(d) == d:
         d = [int(d[i+1]) - int(d[i]) for i in range(len(d)-1)]
         step += 1
-        D[step] =d
+        D.append(d)
 
     while step > 0:
-        # Part 1
-        #D[step-1].append(int(D[step-1][-1]) + int(D[step][-1]))
-        # Part 2
+        D[step-1].append(int(D[step-1][-1]) + int(D[step][-1]))
         D[step-1].insert(0, int(D[step-1][0]) - int(D[step][0]))
         step -= 1
 
-    # Part 1
-    #sum += D[0][-1]
-    # Part 2
-    sum += D[0][0]
+    sum += D[0][-1]
+    sum2 += D[0][0]
 
 print(sum)
+print(sum2)
